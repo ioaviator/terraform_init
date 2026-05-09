8cfcdaf41ded2b2a4021a31c797d5773ee56591d
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-09 10:24:21 +0100

    terraform

--- Azure_tf/backend.tf
+++ Azure_tf/backend.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "tf-backend"
-    storage_account_name = "tfstorage22"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "tf-backend"
+  #   storage_account_name = "tfstorage22"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
 }
